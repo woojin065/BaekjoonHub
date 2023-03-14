561e0601a042c9996fa99c0ff4862531ab191897
woojin065 <123820750+woojin065@users.noreply.github.com>
2023-03-14 18:53:10 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -1,32 +1,14 @@
 #include <iostream>
 #include <vector>
+#include<cstdio>
 using namespace std;
 
-int arr[41][2];
-//0 1 1 2 3 5 
-void fibo(int n){
-  arr[0][0]=1;
-  arr[0][1]=0;
-  arr[1][0]=0;
-  arr[1][1]=1;
-  for(int i=2;i<=n;i++){
-    arr[i][0]=arr[i-1][0]+arr[i-2][0];
-    arr[i][1]=arr[i-1][1]+arr[i-2][1];
-  }
-}
-int main() {
-	int n;
-	cin >> n;
-
-	int max = 0;
-	vector<int> p(n, 0);
-	for (int i = 0; i < n; i++) {
-		cin >> p[i];
-		max = max < p[i] ? p[i] : max;
-	}
-
-  fibo(max);
-	for (int i = 0; i < n; i++) {
-    cout<<arr[p[i]][0]<<" "<<arr[p[i]][1]<<endl;
+int main(){
+	int f[60]={1,0},T,a;
+	for(a=0;a<50;a++)f[a+2]=f[a+1]+f[a];
+	scanf("%d",&T);
+	while(T--){
+	scanf("%d",&a);
+	printf("%d %d\n",f[a],f[a+1]);
 	}
 }
