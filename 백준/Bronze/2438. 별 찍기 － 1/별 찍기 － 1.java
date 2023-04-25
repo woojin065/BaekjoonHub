ce09cbcce171922bef405b16f6b1d7bdcbbec044
woojin065 <123820750+woojin065@users.noreply.github.com>
2023-04-25 14:18:51 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 304 ms, Memory: 18084 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner s = new Scanner(System.in);
+       int n = s.nextInt();
+       for(int i=1;i<=n;i++){
+           for(int j=n-i;j<n;j++){
+               System.out.print("*");
+           }
+           System.out.println("");
+       }
+    }
+}
