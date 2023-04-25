f8210352d7962a79fc1cfe7ef27a081b8fe3b842
woojin065 <123820750+woojin065@users.noreply.github.com>
2023-04-25 14:19:27 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 512 ms, Memory: 21864 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner s = new Scanner(System.in);
+       int n = s.nextInt();
+       for(int i=0;i<n;i++){
+           for(int j=0;j<2*n;j++){
+               if(j>n+i-1) break;
+               if(j<n-i-1) System.out.print(" ");
+               else System.out.print("*");
+           }
+           if(i!=n-1) System.out.println("");
+       }
+    }
+}
