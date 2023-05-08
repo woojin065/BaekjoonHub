ece183fef104e2c244f334b937576ae15e1787a7
woojin065 <123820750+woojin065@users.noreply.github.com>
2023-05-08 18:43:18 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/62048. 멀쩡한 사각형/멀쩡한 사각형.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(int w, int h) {
+    long long answer = 0;
+
+    for(int i=1;i<w;i++)
+    {
+
+        long long n = ((double)h * i)/w;
+        answer+=n;
+    }
+    answer*=2;
+    return answer;
+}
