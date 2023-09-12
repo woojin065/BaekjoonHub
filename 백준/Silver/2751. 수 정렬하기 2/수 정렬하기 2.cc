4f67c4383c0a72701d83fa9baaf9baf608ef58b8
woojin065 <123820750+woojin065@users.noreply.github.com>
2023-09-13 02:18:32 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 276 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -1,28 +1,27 @@
 #include <iostream>
-#include <stdio.h>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int compare(int a, int b) {
-	return a < b;
-}
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	int n;
-	vector<int> arr;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		int temp;
-		cin >> temp;
-		arr.push_back(temp);
-	}
-	sort(arr.begin(), arr.end(), compare);
+    int n;
+    cin >> n;
 
-	for (int i = 0; i < n; i++)
-		cout << arr[i] << '\n';
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr.begin(), arr.end());
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << '\n';
+    }
+
+    return 0;
 }
