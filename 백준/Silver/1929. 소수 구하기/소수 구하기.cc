c6a941055be65d5e135be6eefb2f63ce4ed14db4
woojin065 <123820750+woojin065@users.noreply.github.com>
2023-09-22 00:53:18 +0900

    [Silver III] Title: 소수 구하기, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,25 +1,28 @@
 #include <iostream>
-#include <stdio.h>
 #include <vector>
-#include <algorithm>
-#include <cmath>
 using namespace std;
 
 int main()
 {
-	int n, m;
-	cin >> n >> m;
+    int n, m;
+    cin >> n >> m;
 
-	for (int i = n; i <= m; i++) {
-		int flag = 1;
-		if (i == 1)continue;
-		for (int j = 2; j <= sqrt((double)i); j++) {
-			if (i % j == 0) {
-				flag = 0;
-				break;
-			}
-		}
-		if (flag || i==2 )
-			printf("%d\n", i);
-	}
+    vector<bool> isPrime(m + 1, true);
+    isPrime[0] = isPrime[1] = false;
+
+    for (int i = 2; i * i <= m; ++i) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= m; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    for (int i = n; i <= m; ++i) {
+        if (isPrime[i]) {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
 }
