9aa33473cdf88e6c8aa4a353c726442129f7da6b
woojin065 <123820750+woojin065@users.noreply.github.com>
2023-10-25 17:47:03 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	if ((n % 4 == 0 && n % 100 != 0) || n % 400 == 0) {
+		cout << 1;
+	}
+	else {
+		cout << 0;
+	}
+}
