d49da31f3df7605343c687e71c41330979332988
woojin065 <123820750+woojin065@users.noreply.github.com>
2024-04-11 15:41:56 +0900

    [level 3] Title: 주사위 고르기, Time: 5417.65 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -0,0 +1,22 @@
+# 참고함
+from itertools import combinations, product
+from bisect import bisect_left
+
+def solution(dices):
+    dic = {}
+    L = len(dices)
+    for A_index_combi in combinations(range(L), L//2):
+        B_index_combi = [i for i in range(L) if i not in A_index_combi]
+    
+        A, B = [], []
+        for order_product in product(range(6), repeat=L//2):
+            A.append(sum(dices[i][j] for i, j in zip(A_index_combi, order_product)))
+            B.append(sum(dices[i][j] for i, j in zip(B_index_combi, order_product)))
+        B.sort()
+
+        wins = sum(bisect_left(B, num) for num in A)
+        dic[wins] = list(A_index_combi)
+
+    max_key = max(dic.keys())
+
+    return [x+1 for x in dic[max_key]]
