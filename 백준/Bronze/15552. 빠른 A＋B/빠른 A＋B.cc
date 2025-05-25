dd61dde5a004f55f69b8b0c8ddf293e54c3ab8e3
woojin065 <123820750+woojin065@users.noreply.github.com>
2025-05-25 12:49:02 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 376 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+using namespace std;
+int main() {
+    int T;
+    cin >> T;
+    while(T--){
+        int a, b;
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a+b);
+    }
+}
