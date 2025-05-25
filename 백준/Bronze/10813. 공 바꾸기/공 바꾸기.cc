505a1d7398103c2ee87adb736dfbe19195742e91
woojin065 <123820750+woojin065@users.noreply.github.com>
2025-05-25 13:09:49 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+using namespace std;
+int main() {
+    int N, M;
+    cin >> N >> M;
+    int arr[101];
+    for(int i=1;i<=N;i++)
+        arr[i]=i;
+    for(int i=0;i<M;i++){
+        int a, b;
+        cin >>a>>b;
+        int temp = arr[a];
+        arr[a]=arr[b];
+        arr[b]=temp;
+    }
+    for(int i=1;i<=N;i++)
+        cout << arr[i] << " ";
+}
